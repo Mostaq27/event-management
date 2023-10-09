3be9eab5661798a89899d9185d47acc53f7ef4ed
Mostaq27 <mostaq99muzahid@gmail.com>
2023-10-09 22:52:10 +0600

    updated md file

--- src/components/page/AllEvents/AllEvents.jsx
+++ src/components/page/AllEvents/AllEvents.jsx
@@ -1,8 +1,8 @@
- const AllEvents =() => {
-    return (
-        <div>
-            <h1>this is all events</h1>
-        </div>
-    );
- };
- export default AllEvents;
+//  const AllEvents =() => {
+//     return (
+//         <div>
+//             <h1>this is all events</h1>
+//         </div>
+//     );
+//  };
+//  export default AllEvents;
